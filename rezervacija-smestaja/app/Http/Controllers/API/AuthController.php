73aabbaee3335d26078a7d8febe9921c6cce39ba
sara-slavkovic <saraslavkovic3@gmail.com>
2024-07-08 02:38:05 +0200

    Izmenjen kontroler za autorizaciju

--- rezervacija-smestaja/app/Http/Controllers/API/AuthController.php
+++ rezervacija-smestaja/app/Http/Controllers/API/AuthController.php
@@ -1,14 +1,14 @@
 <?php
-
+ 
 namespace App\Http\Controllers\API;
-
+ 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
-
+ 
 class AuthController extends Controller
 {
     public function register(Request $request)
@@ -18,45 +18,62 @@ class AuthController extends Controller
             'email' => 'required|string|max:255|email|unique:users',
             'password' => 'required|string|min:8',
         ]);
-
+ 
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json(['Registration failed', $validator->errors()]);
         }
-
+ 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
+            'role' => 'user'
         ]);
-
+ 
         $token = $user->createToken('auth_token')->plainTextToken;
+ 
         return response()->json([
             'data' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer'
         ]);
     }
-
+ 
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+        $credentials = $request->only('email', 'password');
+ 
+        $user = User::where('email', $credentials['email'])->first();
+ 
+        if ($user) {
+ 
+            if (Hash::check($credentials['password'], $user->password)) {
+ 
+                $token = $user->createToken('auth_token')->plainTextToken;
+                $role = $user->role;
+ 
+                $response = [
+                    'user' => $user,
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'role' => $role
+                ];
+ 
+                return response()->json($response, 201);
+            } else {
+                return response()->json(['message' => 'Wrong password'], 404);
+            }
+        } else {
+            return response()->json(['message' => 'There is no user with this email address'], 404);
         }
-
-        $user = User::where('email', $request['email'])->firstOrFail();
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => $user->name . ' logged in',
-            'access_token' => $token,
-            'token_type' => 'Bearer'
-        ]);
     }
-
-    public function logout()
+ 
+ 
+    public function logout(Request $request)
     {
-        auth()->user()->tokens()->delete();
-        return response()->json(['You have successfully logged out!']);
+        $user = $request->user();
+ 
+        $user->tokens()->delete();
+        return ['message' => 'You have successfully logged out'];
     }
 }
